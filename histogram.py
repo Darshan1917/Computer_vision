9d4dca922f4f7640fcd38d0fe6a6b25b0c8cad14
Darshan Umapathi <dumapath@igategegdc.com>
2018-02-26 22:14:38 +0530

    read me

--- histogram.py
+++ histogram.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import dlib
 import numpy as np 
 import matplotlib.pyplot as plt
 
@@ -9,6 +10,10 @@ import matplotlib.pyplot as plt
 image = cv2.imread('bird.jpg',0)
 #cv2.imshow('image',image)
 cv2.imshow('image',image)
+
+hist = cv2.calcHist([image],[0],None,[256],[0,256])
+plt.hist(image.ravel(),256,[0,256])
+plt.show()
 
 
 cv2.waitKey(0)  
