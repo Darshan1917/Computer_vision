4da9e549d6c937b8ee19e8bc6de4fb05a64e3fb6
Darshan Umapathi <dumapath@igategegdc.com>
2018-02-27 22:24:53 +0530

    changes

--- histogram.py
+++ histogram.py
@@ -7,17 +7,24 @@ import matplotlib.pyplot as plt
 
 
 
-image = cv2.imread('bird.jpg',0)
+image = cv2.imread('forest.jpg')
 #cv2.imshow('image',image)
 cv2.imshow('image',image)
-
-hist = cv2.calcHist([image],[0],None,[256],[0,256])
-plt.hist(image.ravel(),256,[0,256])
+#hist = cv2.calcHist([image],0,None,[256],[0,256])
+#colour pixel value
+color = ('b','g','r')
+for i,col in enumerate(color):
+    hist = cv2.calcHist([image],[i],None,[256],[0,256])
+    plt.plot(hist,color = col)
+    plt.xlim([0,256])
 plt.show()
+#plt.hist(image.ravel(),256,[0,256])
+#plt.show()
+
 
 
 cv2.waitKey(0)  
 cv2.destroyAllWindows()
-
+    
 
 
